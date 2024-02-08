ae49ae68cb344f35b48e19706edc136201f01cf7
Lucas <smoooki44@gmail.com>
2024-02-08 23:09:08 +0200

    added card, title, when last update, but without data

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,14 @@ import { Card } from "./components/Card"
 function App() {
   return (
     <>
-      <Card />
-    <div className="bg-amber-600 w-[60px] h-[60px] flex justify-center self-center rounded-full cursor-pointer absolute bottom-4 right-3"><span className="text-white text-5xl">+</span></div>
+      <h1 className="text-7xl font-thin text-center">World Weather</h1>
+      <p className="text-gray-500 font-normal mt-4 text-center">Keep track of the weather in the place you want</p>
+      <div className="flex flex-wrap justify-center items-center mt-20">
+        <Card />
+      </div>
+    <div className="bg-amber-600 w-[60px] h-[60px] flex justify-center self-center rounded-full cursor-pointer fixed bottom-4 right-3"><span className="font-thin text-white text-5xl">+</span></div>
     </>
   )
 }
 
-export default App
+export default App;
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,53 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 
 
 interface CardProps {
-
+    city: string,
+    country: string,
+    weather: string,
+    temperature: number,
+    humidity: string
 };
 
-export const Card: FC<CardProps> = () => {
+export const Card: FC<CardProps> = ({ city, country, weather, temperature, humidity }) => {
+    const [minutes, setMinutes] = useState(0);
+
+    const refreshPage = () => {
+        window.location.reload();
+    }
+
+    const reloadCheck = () => {
+        setTimeout(() => {
+            setMinutes(minutes + 1);
+        }, 60000);
+    }
+
+    useEffect(() => {
+        reloadCheck();
+    }, []);
+
     return (
-        <article>
-            
+        <article className='w-[600px] h-[300px] border-2 shadow-2xl mr-10 mb-10 flex justify-between flex-col p-3'>
+            <div>
+                <h1 className='font-semibold text-3xl'>{ city }City</h1>
+                <span>{ country }Country</span>
+            </div>
+            <div className='flex items-center justify-between border-b-2 rounded border-gray-300 w-full'>
+                <span>Weather</span>
+                <span>{ weather }Text</span>
+            </div>
+            <div className='flex items-center justify-between border-b-2 rounded border-gray-300 w-full'>
+                <span>Temperature</span>
+                <span>{ weather }Text</span>
+            </div>
+            <div className='flex items-center justify-between border-b-2 rounded border-gray-300 w-full'>
+                <span>Humidity</span>
+                <span>{ weather }Text</span>
+            </div>
+            <span className='font-semibold ml-auto text-gray-500'>
+                { minutes === 0 ? 'last update just now' : `last update ${ minutes } minute ago` }
+            </span>
+            <button className='text-amber-600 font-semibold ml-auto' onClick={refreshPage}>RELOAD</button>
         </article>
     )
 }
