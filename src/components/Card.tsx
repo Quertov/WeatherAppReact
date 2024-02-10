6bc2f17c003f91f15656e22f5f8e17dcc91daec0
Lucas <smoooki44@gmail.com>
2024-02-10 03:06:32 +0200

    almost finish (need to catch wrong input)

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,7 @@
 import { FC, useEffect, useState } from 'react';
 import axios from 'axios';
 import { IWeatherInfo } from '../components/types';
+import { Loading } from './Loading';
 
 
 interface CardProps {
@@ -12,6 +13,8 @@ export const Card: FC<CardProps> = ({ city }) => {
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
     const [minutes, setMinutes] = useState<number>(0);
+    const [isLoading, setLoading] = useState(false);
+    const [isError, setError] = useState(false);
 
     const [country, setCountry] = useState('');
     const [weather, setWeather] = useState('');
@@ -35,6 +38,7 @@ export const Card: FC<CardProps> = ({ city }) => {
     }
 
     const fetchWeather = async () => {
+        setLoading(true);
         try {
             const response = await axios.get<IWeatherInfo>(apiUrl);
             const data = await response.data;
@@ -43,7 +47,11 @@ export const Card: FC<CardProps> = ({ city }) => {
             setWeather(data.weather[0].main);
             setTemperature(data.main.temp);
             setHumidity(data.main.humidity);
+            setLoading(false);
+            setError(false);
         } catch (error) {
+            setLoading(false);
+            setError(true);
             console.log(`Error:\n   ${error}`);
         }
     }
@@ -55,27 +63,32 @@ export const Card: FC<CardProps> = ({ city }) => {
 
     return (
         <>
-            <article className='w-[600px] h-[300px] border-2 shadow-2xl mr-10 mb-10 flex justify-between flex-col p-3'>
-                <div>
-                    <h1 className='font-semibold text-3xl'>{ city.charAt(0).toUpperCase() + city.slice(1).toLowerCase() }</h1>
-                    <span>{ country }</span>
-                </div>
-                <div className='flex items-center justify-between border-b-2 rounded border-gray-300 w-full'>
-                    <span>Weather</span>
-                    <span>{ weather }</span>
-                </div>
-                <div className='flex items-center justify-between border-b-2 rounded border-gray-300 w-full'>
-                    <span>Temperature</span>
-                    <span>{ Math.round(temperature - 273.15) }&#176;C</span>
-                </div>
-                <div className='flex items-center justify-between border-b-2 rounded border-gray-300 w-full'>
-                    <span>Humidity</span>
-                    <span>{ humidity }%</span>
-                </div>
-                <button className='text-amber-600 font-semibold ml-auto' onClick={refreshPage}>RELOAD</button>
-            </article>
+            { isLoading 
+            ? <Loading /> 
+            : <>
+                <article className='w-[600px] h-[300px] border-2 shadow-2xl mr-10 mb-10 flex justify-between flex-col p-3'>
+                    <div>
+                        <h1 className='font-semibold text-3xl'>{ city.charAt(0).toUpperCase() + city.slice(1).toLowerCase() }</h1>
+                        <span>{ country }</span>
+                    </div>
+                    <div className='flex items-center justify-between border-b-2 rounded border-gray-300 w-full'>
+                        <span>Weather</span>
+                        <span>{ weather }</span>
+                    </div>
+                    <div className='flex items-center justify-between border-b-2 rounded border-gray-300 w-full'>
+                        <span>Temperature</span>
+                        <span>{ Math.round(temperature - 273.15) }&#176;C</span>
+                    </div>
+                    <div className='flex items-center justify-between border-b-2 rounded border-gray-300 w-full'>
+                        <span>Humidity</span>
+                        <span>{ humidity }%</span>
+                    </div>
+                    <button className='text-amber-600 font-semibold ml-auto' onClick={refreshPage}>RELOAD</button>
+                </article>
+                
+            </> }
             <span className='font-normal text-2xl ml-auto text-gray-500 fixed bottom-4 left-3'>
-                { minutes === 0 ? 'last update just now' : `last update ${ minutes } minute ago` }
+                    { minutes === 0 ? 'last update just now' : `last update ${ minutes } minute ago` }
             </span>
         </>
     )
---
+++ src/components/Loading.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+export const Loading: FC = () => {
+    return (
+        <span className="font-thin text-3xl text-black">Loading...</span>
+    )
+}
